8d3fbe695771003aed42e6b17cd5467b663d6cae
Bruno Machado <brunoalvirubro@gmail.com>
2013-02-19 23:51:51 -0300

    Adiciona modelos User, Space e Content

---
+++ db/migrate/20130220024920_create_contents.rb
@@ -0,0 +1,14 @@
+class CreateContents < ActiveRecord::Migration
+  def self.up
+    create_table :contents do |t|
+      t.string :name
+      t.string :description
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :contents
+  end
+end
