9e535e54a634095a4cb186b5be1f2e00987396cc
jessicabarbalho <jessica@redu.com.br>
2013-04-02 22:02:40 -0300

    Adicionando factory_girl_rails ao repositório

---
+++ spec/factories/content_factory.rb
@@ -0,0 +1,7 @@
+FactoryGirl.define do
+  factory :content do
+    name "Link"
+    link "http://www.redu.com.br"
+    kind "website"
+  end
+end
---
+++ spec/factories/user_factory.rb
@@ -0,0 +1,8 @@
+FactoryGirl.define do
+  factory :user do
+    name "User"
+    last_name "Name"
+    email "content@gmail.com"
+    token "777777777777"
+  end
+end
