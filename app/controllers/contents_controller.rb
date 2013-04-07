8b49591b192ee9858e4bfe25abda02252cb493d5
Bruno Machado <brunoalvirubro@gmail.com>
2013-04-07 15:29:42 -0300

    Adiciona cabeçalho com título e abas as páginas

--- app/controllers/contents_controller.rb
+++ app/controllers/contents_controller.rb
@@ -2,5 +2,9 @@ class ContentsController < ActionController::Base
   def index
     @user = User.find(params[:user_id].to_s)
     @contents = @user.contents
+
+    respond_to do |format|
+      format.html { render :layout => 'application' }
+    end
   end
 end
