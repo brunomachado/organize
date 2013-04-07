d46e04b15994a971353905bc7f3c62dedf79a217
Bruno Machado <brunoalvirubro@gmail.com>
2013-04-07 15:33:16 -0300

    Adiciona o Typekit ao projeto

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,2 +1,19 @@
 // Place your application-specific JavaScript functions and classes here
 // This file is automatically included by javascript_include_tag :defaults
+
+$.cachedScript = function(url) {
+  options = {
+    dataType: "script",
+    cache: true,
+    url: url
+  };
+
+  return $.ajax(options);
+};
+
+$.cachedScript("http://use.typekit.com/lpo4rgu.js").done(function() {
+  try {
+    Typekit.load();
+    $(".typekit-badge").css("left", "0");
+  }catch(e){}
+});
